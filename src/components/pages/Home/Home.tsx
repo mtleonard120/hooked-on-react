bdec97754405fd21b3480fe5e8fd5017bac916e4
Michael Leonard <michael.leonard@cqlcorp.local>
2020-04-13 21:43:44 -0400

    Update Homepage description

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,25 +1,23 @@
 import React from 'react'
 
-// packages
-
-// hooks
-
-// components
-
-// styles
-// import styles from './Home.module.scss'
-
-// utils
-
 // types
 export interface IHomeProps {}
 
 // primary component
-export const Home: React.FC<IHomeProps> = props => {
-    return (
-        <>
-            <h1>Hooked on React</h1>
-            <p>This app is a companion to my Hooked on React presentation found ***INSERT LINK HERE***</p>
-        </>
-    )
-}
+export const Home: React.FC<IHomeProps> = props => (
+    <>
+        <h1>Hooked on React</h1>
+        <p>
+            This app is a companion to my Hooked on React presentation found{' '}
+            <a
+                href='https://docs.google.com/presentation/d/1Hhs20BZf844ebt7yFC9aSor2gOWUS7WTUJxhT2DTRsE/edit?usp=sharing'
+                target='_blank'
+                rel='noopener noreferrer'
+            >
+                here
+            </a>
+            . The links in the top nav demonstrate various behaviors of hooks. Pop open your react dev tools and play
+            around with them!
+        </p>
+    </>
+)
